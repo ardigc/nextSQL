cef7526b224cce0ffc4b7fff24854e0a96cb56cb
ardigc <130089987+ardigc@users.noreply.github.com>
2023-05-17 17:15:36 +0000

    pruebas

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,13 @@ export default async function Home() {
   return (
   
     <div>
-      { user.rows.map(row=> <h1> {row.name} {row.email}</h1>)}
-      <h1></h1>
+      {/* { user.rows.map(row=> <h1> {row.name} {row.email}</h1>)} */}
+     <div className="fixed top-0 w-screen h-12 flex items-center justify-center bg-blue-200">
+     <h1>Eshop</h1>
+      </div>
+      <div className="relative top-12 bg-slate-200">
       {result.rows.map(row => <Product {...row} />)}
+      </div>
     </div>
   )
 }
