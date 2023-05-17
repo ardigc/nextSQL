39e104f3f2bf2420e20195a09c0e047fc41f49ce
ardigc <130089987+ardigc@users.noreply.github.com>
2023-05-17 19:50:34 +0000

    mensaje

--- app/MyClientComponent.tsx
+++ app/MyClientComponent.tsx
@@ -1,19 +1,24 @@
-'use client'
+'use client';
 
-export function Product({name, description, price}: {name: string, description:string, price:number}) {
-    
-    return <div className="border border-slate-400">
-        <div className="flex justify-center">
-        {name}
-        </div>
-        <div className="flex justify-center items-center">
-        {description}
-        </div>
-        <div className="flex justify-end items-center">
-<button className="px-1 border bg-slate-500 rounded-3xl mx-5">Añadir al carrito</button>
-<div className="w-16 flex justify-end me-2">
-{price} €
-</div>
-</div>
-        </div>
+export function Product({
+  name,
+  description,
+  price,
+}: {
+  name: string;
+  description: string;
+  price: number;
+}) {
+  return (
+    <div className="border border-slate-400">
+      <div className="flex justify-center">{name}</div>
+      <div className="flex justify-center items-center">{description}</div>
+      <div className="flex justify-end items-center">
+        <button className="px-1 border bg-slate-500 rounded-3xl mx-5">
+          Añadir al carrito
+        </button>
+        <div className="w-16 flex justify-end me-2">{price} €</div>
+      </div>
+    </div>
+  );
 }
