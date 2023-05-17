421b5fa6e77817da7d2a8d7cf94acef1711a525c
JuanMOlivas <41582680+CerealPlayer@users.noreply.github.com>
2023-05-17 11:05:26 +0000

    Add routing

---
+++ app/api/cart/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest } from "next/server";
+import { pool } from '@/lib/server/pg'
+
+export async function GET() {
+    return new Response("Hello, world!", {status: 200})
+}
+
+// Handler para fetch con method POST a esta ruta /api/cart
+export async function POST(req: NextRequest) {
+    // Body del fetch en el front
+    const body = await req.json()
+    pool.query('query para insertar en db')
+
+    // Cuando terminas retornas response
+    return new Response(JSON.stringify({lo: 'que quieras'}), {status: 200})
+}
---
+++ app/hello/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { POST } from "../api/cart/route";
+
+export default function Hello() {
+    const clickHandler = async () => {
+        const response = await fetch('/api/cart');
+        const text = await response.text()
+        console.log(text)
+    }
+    return <button onClick={clickHandler}>Hello</button>
+}
