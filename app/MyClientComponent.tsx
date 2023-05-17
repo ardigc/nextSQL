81a0c5fbc7f82f5b1a2812c07e7e3132521c164a
ardigc <130089987+ardigc@users.noreply.github.com>
2023-05-17 16:21:26 +0000

    pruebas

--- app/MyClientComponent.tsx
+++ app/MyClientComponent.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-export function Product({name, description}: {name: string, description:string}) {
-    console.log(name, description)
-    return <div>{name} - {description}</div>
+export function Product({name, description, price}: {name: string, description:string, price:number}) {
+    // console.log(name, description)
+    return <div>{name} - {description} -{price} €</div>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,13 @@ import { Product } from "./MyClientComponent"
 
 export default async function Home() {
   const result = await pool.query('SELECT * FROM products;')
+  const user = await pool.query('SELECT * FROM users_info WHERE id=1')
+  console.log(user)
   return (
+  
     <div>
+      { user.rows.map(row=> <h1> {row.name} {row.email}</h1>)}
+      <h1></h1>
       {result.rows.map(row => <Product {...row} />)}
     </div>
   )
