b99b7fbe312658bd46b9897e74043f181dcc4a9a
ardigc <130089987+ardigc@users.noreply.github.com>
2023-05-17 19:10:01 +0000

    use state iniciar sesion

--- app/Sesion.tsx
+++ app/Sesion.tsx
@@ -1,7 +1,16 @@
+'use client'
+import { MouseEventHandler, useState } from "react"
+
 export default function Sesion() {
+const [inLogging, setInLogging]=useState(false)
+const clickHandler: MouseEventHandler<HTMLButtonElement>=((ev)=>{
+    setInLogging(true)
+})
+
    return ( <div className="absolute flex items-center justify-end w-screen h-12">
-    
-    <button className=" border rounded-3xl bg-slate-500 px-1 mx-10">Iniciar sesion</button>
-    
+    <button onClick={clickHandler} className=" border rounded-3xl bg-slate-500 px-1 mx-10">Iniciar sesion</button>
+   {inLogging && <div>
+    HOLA
+    </div>}
    </div>)
 }
--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -2,11 +2,11 @@
 
 import { POST } from "../api/cart/route";
 
-export default function Hello() {
-    const clickHandler = async () => {
-        const response = await fetch('/api/cart');
-        const text = await response.text()
-        console.log(text)
-    }
-    return <button onClick={clickHandler}>Hello</button>
-}
+// export default function Hello() {
+//     const clickHandler = async () => {
+//         const response = await fetch('/api/cart');
+//         const text = await response.text()
+//         console.log(text)
+//     }
+//     return <button onClick={clickHandler}>Hello</button>
+// }
