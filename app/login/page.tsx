7a8e2ca3f04eec76f416172828ea7fad3b476e6b
JuanMOlivas <41582680+CerealPlayer@users.noreply.github.com>
2023-05-18 10:34:56 +0000

    Add prettier again

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,19 @@
-'use client'
+'use client';
 
 export default function SignIn() {
-    return <div className="top-7 border rounded-lg min-w-fit right-9 bg-blue-300 ">
-    <form className="px-3">
-      <label>Correo electronico</label>
-      <input></input>
-      <label>Contraseña</label>
-      <input></input>
-      <div className="flex justify-end">
-        <button className="m-1 bg-slate-500 border rounded-full px-1">
-          Summit
-        </button>
-      </div>
-    </form>
-  </div>
+  return (
+    <div className="top-7 border rounded-lg min-w-fit right-9 bg-blue-300 ">
+      <form className="px-3">
+        <label>Correo electronico</label>
+        <input></input>
+        <label>Contraseña</label>
+        <input type="password"></input>
+        <div className="flex justify-end">
+          <button className="m-1 bg-slate-500 border rounded-full px-1">
+            Summit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,15 @@ export default async function Home() {
       <div className="z-50 fixed top-0 w-screen h-12 flex items-center justify-center bg-blue-200">
         <h1>Eshop</h1>
         <div className="absolute top-7 border rounded-lg min-w-fit right-9 bg-blue-300 ">
-            <div className="flex justify-end text-blue-400 px-2">
-        <Link href="/login" className='border rounded-3xl bg-slate-500 px-1 mx-10'>Iniciar sesión</Link>
-      
-      </div>
-      </div>
+          <div className="flex justify-end text-blue-400 px-2">
+            <Link
+              href="/login"
+              className="border rounded-3xl bg-slate-500 px-1 mx-10"
+            >
+              Iniciar sesión
+            </Link>
+          </div>
+        </div>
       </div>
       <div className="relative top-12 grid grid-cols-2 bg-slate-200">
         {result.rows.map((row) => (
