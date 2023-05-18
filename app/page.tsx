c78c517f9bee1590d09d6e8273cc890d4c5f2a08
ardigc <agordo.95@gmail.com>
2023-05-18 10:28:25 +0200

    mover sesion a components

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
-import { pool } from "@/lib/server/pg"
-import { Product } from "./MyClientComponent"
-import Sesion from "./Sesion"
+import { pool } from '@/lib/server/pg';
+import { Product } from './MyClientComponent';
+import Sesion from '../components/Sesion';
 
 export default async function Home() {
-  const result = await pool.query('SELECT * FROM products;')
-  const user = await pool.query('SELECT * FROM users_info WHERE id=1')
-  console.log(user)
+  const result = await pool.query('SELECT * FROM products;');
+  const user = await pool.query('SELECT * FROM users_info WHERE id=1');
+  console.log(user);
   return (
-  
     <div>
       {/* { user.rows.map(row=> <h1> {row.name} {row.email}</h1>)} */}
-     <div className="fixed top-0 w-screen h-12 flex items-center justify-center bg-blue-200">
-     <h1>Eshop</h1>
-     <Sesion />
+      <div className="fixed top-0 w-screen h-12 flex items-center justify-center bg-blue-200">
+        <h1>Eshop</h1>
+        <Sesion />
       </div>
       <div className="relative top-12 grid grid-cols-2 bg-slate-200">
-      {result.rows.map(row => <Product {...row} />)}
+        {result.rows.map((row) => (
+          <Product {...row} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
