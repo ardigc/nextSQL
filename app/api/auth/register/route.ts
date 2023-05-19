654bf27a8baebf88dd708fd9beefd426b686440d
JuanMOlivas <41582680+CerealPlayer@users.noreply.github.com>
2023-05-19 09:52:25 +0000

    Hash pass

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,8 +1,11 @@
 import { NextRequest } from 'next/server';
 import { pool } from '@/lib/server/pg';
+import { hash } from 'bcrypt';
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
+  const unhasedPass = body.pass;
+  const hashedPass = await hash(unhasedPass, 10);
   const query =
     'INSERT INTO users_info(name,email, subname, phone, password) VALUES ($1, $2, $3, $4, $5)';
   const parameters = [
@@ -10,7 +13,7 @@ export async function POST(req: NextRequest) {
     body.email,
     body.subname,
     body.phone,
-    body.pass,
+    hashedPass,
   ];
   const result = await pool.query(query, parameters);
   console.log(result);
