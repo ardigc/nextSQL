36486a1c4a0a992d24af8487287eed9a92207927
ardigc <130089987+ardigc@users.noreply.github.com>
2023-05-22 01:01:44 +0000

    añadido single component

--- components/SingleProduct.tsx
+++ components/SingleProduct.tsx
@@ -1,20 +1,39 @@
 'use client';
 
+import { MouseEventHandler } from 'react';
+
 export default function SingleProduct({
   name,
   description,
   price,
+  id,
 }: {
   name: string;
   description: string;
   price: number;
+  id: number;
 }) {
+  const clickHandler: MouseEventHandler<HTMLButtonElement> = async (ev) => {
+    ev.preventDefault();
+    const response = await fetch('/api/cart', {
+      method: 'POST',
+      body: JSON.stringify({ id }),
+      credentials: 'include',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    window.location.reload();
+  };
   return (
     <div className="grid grid-cols-1 border border-blue-400 bg-blue-200">
       <div className="flex justify-center">{name}</div>
       <div className="flex justify-center items-center">{description}</div>
       <div className="flex justify-end items-center">
-        <button className="px-1 border bg-blue-400 rounded-3xl mx-5">
+        <button
+          onClick={clickHandler}
+          className="px-1 border bg-blue-400 rounded-3xl mx-5"
+        >
           Añadir al carrito
         </button>
         <div className="w-16 flex justify-end me-2">{price} €</div>
