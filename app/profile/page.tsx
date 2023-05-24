17472676926d5fc04deac23b4b3347c60914738b
ardigc <130089987+ardigc@users.noreply.github.com>
2023-05-24 20:39:03 +0000

    obteniendo informacion para datos usu

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,8 @@
-export default function Profile() {
+import { pool } from '@/lib/server/pg';
+
+export default async function Profile() {
+  // hacer un select a la informacion
+  const cart = await pool.query('SELECT * FROM user_info WHERE id=');
   return (
     <div className="relative top-12 bg-blue-100 h-screen w-full">hola</div>
   );
