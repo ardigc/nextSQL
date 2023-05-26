2b3f87b4e3bd156b4d7f51dc826f3bccd2291b61
ardigc <agordo.95@gmail.com>
2023-05-26 16:22:15 +0200

    configurando api

---
+++ app/api/payment/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest } from 'next/server';
+import { Stripe } from 'stripe';
+
+const stripe = new Stripe(`${process.env.STRIPE_KEY_SECRET}`, {
+  apiVersion: '2022-11-15',
+});
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  try {
+    const paymentIntent = stripe.paymentIntents.create({
+      amount: body.amount,
+      currency: 'eur',
+      payment_method_types: ['card'],
+      description: body.description,
+    });
+    return new Response(JSON.stringify(paymentIntent), { status: 200 });
+  } catch (error) {
+    throw new Error('fallo en el pago' + error);
+  }
+}
