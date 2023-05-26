64fdc3869b22f669e47fdbf50fbf4fbf688d9104
ardigc <agordo.95@gmail.com>
2023-05-26 16:09:45 +0200

    avnazando con apgo

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -4,17 +4,35 @@ import { FormEventHandler } from 'react';
 import Link from 'next/link';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
+import { CardElement } from '@stripe/react-stripe-js';
 
 export default async function SignIn() {
   const stripe = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_KEY_PUBLIC}`);
+  const cardStyle = {
+    style: {
+      base: {
+        color: 'white',
+        fontSize: '16px',
+        fontSmoothing: 'abtuakuased',
+        '::placeholder': {
+          color: '#c4cld0',
+        },
+      },
+      invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a',
+      },
+    },
+  };
   return (
-    <div className="relative top-12 bg-blue-100 h-screen w-full">
-      <div className="absolute top-7 left-1/2 -translate-x-1/2 border rounded-lg min-w-fit flex justify-center bg-blue-300 shadow-black shadow-2xl">
-        <Elements stripe={stripe}></Elements>
-        {/* <form className="px-3 grid grid-cols-1">
-         
-        </form> */}
+    <Elements stripe={stripe}>
+      <div className="relative top-12 bg-blue-100 h-screen w-full">
+        <div className="absolute top-7 left-1/2 -translate-x-1/2 border rounded-lg w-2/4 flex justify-center bg-blue-300 shadow-black shadow-2xl">
+          <form className="px-3 grid grid-cols-1 w-full">
+            <CardElement id="card-element" />
+          </form>
+        </div>
       </div>
-    </div>
+    </Elements>
   );
 }
