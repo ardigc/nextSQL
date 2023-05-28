d840de7ab0898e55e650b304b529a360cd7094a1
ardigc <130089987+ardigc@users.noreply.github.com>
2023-05-28 15:04:28 +0000

    pruebas para conseguir carrito

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -2,11 +2,29 @@ import OrdersPrev from '@/components/OrderPrev';
 import { pool } from '@/lib/server/pg';
 import { verify } from 'jsonwebtoken';
 import { cookies } from 'next/headers';
+import { Query, QueryResult } from 'pg';
 
+interface Cart {
+  cart_id: number;
+  description: string;
+  id: number;
+  name: string;
+  price: number;
+  product_id: number;
+  qt: number;
+  user_id: number;
+}
 export default async function Orders() {
   const cookiesValue = cookies();
   let user = null;
   let orders = null;
+
+  function totalPrice(products: Array<Cart>) {
+    return products.reduce((total, products) => {
+      const price = products.price * products.qt;
+      return total + price;
+    }, 0);
+  }
   try {
     user = verify(
       cookiesValue.get('token')?.value || '',
@@ -18,16 +36,27 @@ export default async function Orders() {
     orders = await pool.query(
       `SELECT orders.id, orders.user_id, orders.cart_id , users_adress.line, users_adress.postal_code, users_adress.city, users_adress.country, users_adress.marked_as_default FROM orders INNER JOIN users_adress ON orders.adress= users_adress.id WHERE orders.user_id= ${user.id} ORDER BY orders.id DESC`
     );
-
-    console.log(orders.rows);
+    const cart = [];
+    cart.push(
+      orders.rows.map(async (order) => {
+        const cartBeta = await pool.query(
+          'SELECT * FROM carts INNER JOIN cart_items ON carts.id = cart_items.cart_id INNER JOIN products ON products.id = cart_items.product_id  WHERE carts.id=' +
+            order.cart_id +
+            ' ORDER BY product_id DESC'
+        );
+        return cartBeta.rows;
+      })
+    );
+    console.log(cart);
   } catch (error: any) {
     throw error;
   }
+
   return (
     <div className="relative top-12 bg-blue-100 h-screen w-full">
       <div className="w-11/12 max-w-2xl mx-auto border rounded-lg grid grid-cols-1 p-3 relative top-7 justify-center text-center bg-blue-300 shadow-black shadow-2xl  ">
         Tus pedidos
-        {orders.rows.map((order) => (
+        {orders.rows.map((order, index) => (
           <OrdersPrev order={order} />
         ))}
       </div>
--- components/OrderPrev.tsx
+++ components/OrderPrev.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { pool } from '@/lib/server/pg';
 import { useEffect, useState } from 'react';
 
@@ -22,39 +21,28 @@ interface Cart {
   qt: number;
   user_id: number;
 }
-export default function OrdersPrev({ order }: { order: Order }) {
-  //   const [cart, setCart] = useState<Array<Cart>>([]);
+export default function OrdersPrev({
+  order,
+}: {
+  order: Order;
+  cart?: Array<Cart>;
+}) {
   const cartId = order.cart_id;
 
-  //   async function getCart() {
+  function totalPrice(products: Array<Cart>) {
+    return products.reduce((total, products) => {
+      const price = products.price * products.qt;
+      return total + price;
+    }, 0);
+  }
+  async function getCart() {
+    const cart = await pool.query(
+      'SELECT * FROM carts INNER JOIN cart_items ON carts.id = cart_items.cart_id INNER JOIN products ON products.id = cart_items.product_id  WHERE carts.id=' +
+        order.cart_id +
+        ' ORDER BY product_id DESC'
+    );
+  }
 
-  // setCart(data);
-  //   }
-  //   getCart();
-  useEffect(() => {
-    async function totalPrice() {
-      const response = await fetch('/api/order', {
-        method: 'POST',
-        body: JSON.stringify({ cartId }),
-        headers: {
-          'Content-type': 'application/json',
-        },
-      });
-      const data = await response.json();
-      const products = data;
-      console.log('hola');
-      return 'hola';
-
-      // const cart = await pool.query('SELECT * FROM carts INNER JOIN cart_items ON carts.id = cart_items.cart_id INNER JOIN products ON products.id = cart_items.product_id  WHERE carts.id=' +
-      // cart_id +
-      // ' ORDER BY product_id DESC')
-      // return products.reduce((total: number, products: { price: number; qt: number; }) => {
-      //   const price = products.price * products.qt;
-      //   return total + price;
-      // }, 0);
-    }
-    const price = totalPrice();
-  });
   return (
     <button className="border my-1 rounded-lg border-blue-900 hover:bg-blue-500 bg-blue-400">
       <div>
