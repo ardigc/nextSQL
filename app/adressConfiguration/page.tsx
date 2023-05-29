97ecb37db2aecdc61f3b99f0366f38a0229518e7
ardigc <agordo.95@gmail.com>
2023-05-29 12:16:59 +0200

    solucionado direcciones

--- app/adressConfiguration/page.tsx
+++ app/adressConfiguration/page.tsx
@@ -34,38 +34,16 @@ export default async function adressConfiguration() {
   } catch (error: any) {
     console.error('Error al verificar el token:', error.message);
   }
-  // console.log(adressDefault?.rows[0].id);
+  console.log(adress?.rows);
   return (
     <div className="relative top-12 bg-blue-100 h-screen w-full ">
       <AdressProvider initialAdress={adressDefault?.rows[0].id}>
-        {adress?.rowCount === 0 && (
-          <div className="w-11/12 max-w-3xl mx-auto border rounded-lg p-3 relative top-7 justify-center bg-blue-300 shadow-black shadow-2xl">
-            <AdressComponent userId={userId!} />
-          </div>
-        )}
-        {adress?.rows.length! > 0 && (
-          <div className="w-11/12 max-w-3xl mx-auto border rounded-lg p-3 relative top-7 justify-center bg-blue-300 shadow-black shadow-2xl">
-            Tus direcciones:
-            {adress?.rows.map((adress) => (
-              <div className="flex justify-between items-center my-3">
-                <div>Calle: {adress.line} </div>
-                <div>CP: {adress.postal_code} </div>
-                <div>Ciudad: {adress.city} </div>
-                <div>Pais: {adress.country} </div>
-
-                <AdressDefault userId={userId!} adressId={adress.id} />
-              </div>
-            ))}
-            <div className="flex justify-between items-center">
-              <NewAdress userId={userId!} />
-              <Link
-                className="border mt-2 rounded-3xl bg-blue-400 px-2 mx-1"
-                href="/payment"
-              >
-                Pagar
-              </Link>
-            </div>
-          </div>
+        {adress && adressDefault && (
+          <NewAdress
+            adress={adress.rows}
+            userId={userId!}
+            adressDefault={adressDefault?.rows[0]}
+          />
         )}
       </AdressProvider>
     </div>
--- components/NewAdress.tsx
+++ components/NewAdress.tsx
@@ -2,22 +2,72 @@
 
 import { MouseEventHandler, useState } from 'react';
 import AdressComponent from './AdressComponent';
+import Link from 'next/link';
+import AdressDefault from './AdressDefault';
+interface Adress {
+  id: number;
+  user_id: number;
+  line: string;
+  postal_code: number;
+  city: string;
+  country: string;
+  marked_as_default: Date;
+}
 
-export default function NewAdress({ userId }: { userId: number }) {
+export default function NewAdress({
+  userId,
+  adress,
+  adressDefault,
+}: {
+  userId: number;
+  adress: Array<Adress>;
+  adressDefault: Adress;
+}) {
   const [showAdress, setShowAdress] = useState(false);
   const clickHandler: MouseEventHandler<HTMLButtonElement> = (ev) => {
     setShowAdress(!showAdress);
   };
+
   return (
     <div>
       {!showAdress && (
-        <div className="flex justify-end">
-          <button
-            onClick={clickHandler}
-            className="border mt-2 rounded-3xl bg-blue-400 px-2 mx-1"
-          >
-            Enviar a otra direccion
-          </button>
+        <div>
+          {adress.length === 0 && (
+            <div className="w-11/12 max-w-3xl mx-auto border rounded-lg p-3 relative top-7 justify-center bg-blue-300 shadow-black shadow-2xl">
+              <AdressComponent userId={userId!} />
+            </div>
+          )}
+          {adress.length! > 0 && (
+            <div className="w-11/12 max-w-3xl mx-auto border rounded-lg p-3 relative top-7 justify-center bg-blue-300 shadow-black shadow-2xl">
+              Tus direcciones:
+              {adress.map((adress) => (
+                <div className="flex justify-between items-center my-3">
+                  <div>Calle: {adress.line} </div>
+                  <div>CP: {adress.postal_code} </div>
+                  <div>Ciudad: {adress.city} </div>
+                  <div>Pais: {adress.country} </div>
+
+                  <AdressDefault userId={userId!} adressId={adress.id} />
+                </div>
+              ))}
+              <div className="flex justify-between items-center">
+                <div className="flex justify-end">
+                  <button
+                    onClick={clickHandler}
+                    className="border mt-2 rounded-3xl bg-blue-400 px-2 mx-1"
+                  >
+                    Enviar a otra direccion
+                  </button>
+                </div>
+                <Link
+                  className="border mt-2 rounded-3xl bg-blue-400 px-2 mx-1"
+                  href="/payment"
+                >
+                  Pagar
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
       )}
       {showAdress && <AdressComponent userId={userId} />}
