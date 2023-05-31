2dab2a00b4b82308f7a641ab0fd20ea4c80b3f03
ardigc <130089987+ardigc@users.noreply.github.com>
2023-05-31 19:49:56 +0000

    modificando input

--- components/product/ModifyProduct.tsx
+++ components/product/ModifyProduct.tsx
@@ -43,37 +43,56 @@ export default function ModifyProduct({
         <div className="flex justify-center">Descripción</div>
         <div className="flex justify-center">Precio</div>
         <div></div>
-        {products.map((product) => (
-          <>
-            {edit !== product.id && (
-              <>
-                <div className="flex my-2 items-center justify-center">
-                  {product.name}
-                </div>
-                <div className="flex items-center justify-center text-center">
-                  {product.description}
-                </div>
-                <div className="flex items-center justify-center">
-                  {product.price}€
-                </div>
-                <div className="content-center flex justify-center">
-                  <button onClick={(ev) => clickDeleteHandle(ev, product.id)}>
-                    <TrashIconMedium />
-                  </button>
-                  <button onClick={() => setEdit(product.id)}>
-                    <PenIconBig />
-                  </button>
-                </div>
-              </>
-            )}
-            {edit === product.id && (
-              <form>
-                <input defaultValue={product.name}></input>
-              </form>
-            )}
-          </>
-        ))}
       </div>
+
+      {products.map((product) => (
+        <>
+          {edit !== product.id && (
+            <div className="grid grid-cols-[3fr_3fr_3fr_1fr] items-center my-5">
+              <div className="flex my-2 items-center justify-center">
+                {product.name}
+              </div>
+              <div className="flex items-center justify-center text-center">
+                {product.description}
+              </div>
+              <div className="flex items-center justify-center">
+                {product.price}€
+              </div>
+              <div className="content-center flex justify-center">
+                <button onClick={(ev) => clickDeleteHandle(ev, product.id)}>
+                  <TrashIconMedium />
+                </button>
+                <button onClick={() => setEdit(product.id)}>
+                  <PenIconBig />
+                </button>
+              </div>
+            </div>
+          )}
+          {edit === product.id && (
+            <form className="grid grid-cols-[3fr_3fr_3fr_1fr] items-center my-5">
+              <input
+                type="text"
+                name="name"
+                defaultValue={product.name}
+                className="flex my-2 items-center justify-center"
+              ></input>
+              <input
+                type="text"
+                name="descriptiton"
+                defaultValue={product.description}
+                className="flex my-2 items-center justify-center"
+              ></input>
+              <input
+                type="number"
+                name="price"
+                defaultValue={product.price}
+                className="flex my-2 items-center justify-center"
+              ></input>
+              <div></div>
+            </form>
+          )}
+        </>
+      ))}
     </div>
   );
 }
