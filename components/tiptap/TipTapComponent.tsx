910cf4061923abe49dcc4f0262823ee75654e415
ardigc <130089987+ardigc@users.noreply.github.com>
2023-06-01 16:30:37 +0000

    poniendo headings

--- components/tiptap/TipTapComponent.tsx
+++ components/tiptap/TipTapComponent.tsx
@@ -2,12 +2,21 @@
 import Bold from '@tiptap/extension-bold';
 import Strike from '@tiptap/extension-strike';
 import Underline from '@tiptap/extension-underline';
+import Italic from '@tiptap/extension-italic';
+import Heading from '@tiptap/extension-heading';
 
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit, Bold, Strike, Underline],
+    extensions: [
+      StarterKit,
+      Bold,
+      Strike,
+      Underline,
+      Italic,
+      Heading.configure({ levels: [1, 2, 3, 4, 5, 6] }),
+    ],
     content: '<p>Hello World! 🌎️</p>',
   });
   if (!editor) {
@@ -33,6 +42,26 @@ const Tiptap = () => {
       >
         U
       </button>
+      <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        className="border italic w-7 h-7 border-black"
+      >
+        I
+      </button>
+      Heading:
+      <select
+      // onSelect={(ev) =>{
+      //   const value= ev.currentTarget.value
+      //   console.log(value)
+      // editor.chain().focus().toggleHeading({  }).run()}}
+      >
+        <option value={'level: 1'}>H1</option>
+        <option value={2}>H2</option>
+        <option value={3}>H3</option>
+        <option value={4}>H4</option>
+        <option value={5}>H5</option>
+        <option value={6}>H6</option>
+      </select>
       <EditorContent editor={editor} />
     </>
   );
