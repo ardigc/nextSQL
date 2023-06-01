fa63c0136ca9576aa3bb30e168ca67aaea120440
ardigc <130089987+ardigc@users.noreply.github.com>
2023-06-01 16:58:49 +0000

    probando heading

--- components/tiptap/TipTapComponent.tsx
+++ components/tiptap/TipTapComponent.tsx
@@ -50,10 +50,21 @@ const Tiptap = () => {
       </button>
       Heading:
       <select
-        onSelect={(ev) => {
-          const value: Level = parseInt(ev.currentTarget.value);
-          console.log(value);
-          editor.chain().focus().toggleHeading({ level: value }).run();
+        onChange={(ev) => {
+          const value = parseInt(ev.target.value);
+          if (value === 1) {
+            editor.chain().focus().toggleHeading({ level: 1 }).run();
+          } else if (value === 2) {
+            editor.chain().focus().toggleHeading({ level: 2 }).run();
+          } else if (value === 3) {
+            editor.chain().focus().toggleHeading({ level: 3 }).run();
+          } else if (value === 4) {
+            editor.chain().focus().toggleHeading({ level: 4 }).run();
+          } else if (value === 5) {
+            editor.chain().focus().toggleHeading({ level: 5 }).run();
+          } else if (value === 6) {
+            editor.chain().focus().toggleHeading({ level: 6 }).run();
+          }
         }}
       >
         <option value={1}>H1</option>
@@ -63,6 +74,11 @@ const Tiptap = () => {
         <option value={5}>H5</option>
         <option value={6}>H6</option>
       </select>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+      >
+        Heading
+      </button>
       <EditorContent editor={editor} />
     </>
   );
