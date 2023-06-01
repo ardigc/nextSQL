42dd7e5f3ca8b1db42567febc3d1892fc1610f38
ardigc <agordo.95@gmail.com>
2023-06-02 00:42:08 +0200

    añadiendo color

--- components/Icons/Icons.tsx
+++ components/Icons/Icons.tsx
@@ -289,3 +289,47 @@ export function HomeIcon() {
     </div>
   );
 }
+export function UndoIcon() {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-arrow-back-up"
+        width={20}
+        height={20}
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="#2c3e50"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M9 14l-4 -4l4 -4" />
+        <path d="M5 10h11a4 4 0 1 1 0 8h-1" />
+      </svg>
+    </div>
+  );
+}
+export function RedoIcon() {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-arrow-forward-up"
+        width={20}
+        height={20}
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="#2c3e50"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M15 14l4 -4l-4 -4" />
+        <path d="M19 10h-11a4 4 0 1 0 0 8h1" />
+      </svg>
+    </div>
+  );
+}
--- components/tiptap/TipTapComponent.tsx
+++ components/tiptap/TipTapComponent.tsx
@@ -4,6 +4,11 @@ import Strike from '@tiptap/extension-strike';
 import Underline from '@tiptap/extension-underline';
 import Italic from '@tiptap/extension-italic';
 import Heading, { Level } from '@tiptap/extension-heading';
+import FontFamily from '@tiptap/extension-font-family';
+import TextStyle from '@tiptap/extension-text-style';
+import { Color } from '@tiptap/extension-color';
+import Highlight from '@tiptap/extension-highlight';
+import History from '@tiptap/extension-history';
 
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
@@ -14,7 +19,15 @@ const Tiptap = () => {
       Bold,
       Strike,
       Underline,
+      TextStyle,
       Italic,
+      Highlight.configure({ multicolor: true }),
+      FontFamily.configure({
+        types: ['textStyle'],
+      }),
+      Color.configure({
+        types: ['textStyle'],
+      }),
       Heading.configure({
         levels: [1, 2, 3, 4, 5],
       }),
@@ -27,6 +40,18 @@ const Tiptap = () => {
   return (
     <>
       <div className="flex">
+        <button
+          onClick={() => editor.chain().focus().undo().run()}
+          disabled={!editor.can().undo()}
+        >
+          undo
+        </button>
+        <button
+          onClick={() => editor.chain().focus().redo().run()}
+          disabled={!editor.can().redo()}
+        >
+          redo
+        </button>
         <button
           onClick={() => editor.chain().focus().toggleBold().run()}
           className="font-bold italic border  w-7 h-7 border-black"
@@ -53,8 +78,10 @@ const Tiptap = () => {
         </button>
         <div className="ml-2">Heading:</div>
         <select
+          className="border"
           onChange={(ev) => {
             const value = parseInt(ev.currentTarget.value) as Level;
+            console.log(editor);
             editor.chain().focus().toggleHeading({ level: value }).run();
           }}
         >
@@ -63,6 +90,72 @@ const Tiptap = () => {
           <option value={3}>H3</option>
           <option value={4}>H4</option>
           <option value={5}>H5</option>
+        </select>
+        <div className="ml-2">Font:</div>
+        <select
+          className="border"
+          onChange={(ev) => {
+            const value = ev.currentTarget.value;
+
+            if (value === 'unset') {
+              editor.chain().focus().unsetFontFamily().run();
+            }
+            editor.chain().focus().setFontFamily(value).run();
+          }}
+        >
+          <option value={'unset'}>Stock</option>
+          <option value={'Comic Sans MS, Comic Sans'}>Comic Sans</option>
+          <option value={'serif'}>Serif</option>
+          <option value={'monospace'}>Monospace</option>
+          <option value={'cursive'}>Cursive</option>
+        </select>
+        <div className="ml-2">Color:</div>
+        <select
+          className="border "
+          onChange={(ev) => {
+            const value = ev.currentTarget.value;
+
+            editor.chain().focus().setColor(value).run();
+          }}
+        >
+          <option value={'#94a3b8'} className="w-2 h-2 bg-slate-400"></option>
+          <option value={'#6b7280'} className="w-2 h-2 bg-gray-500"></option>
+          <option value={'#ef4444'} className="w-2 h-2 bg-red-500"></option>
+          <option value={'#fb923c'} className="w-2 h-2 bg-orange-400"></option>
+          <option value={'#4ade80'} className="w-2 h-2 bg-green-400"></option>
+          <option value={'#2dd4bf'} className="w-2 h-2 bg-teal-400"></option>
+          <option value={'#22d3ee'} className="w-2 h-2 bg-cyan-400"></option>
+          <option value={'#2563eb'} className="w-2 h-2 bg-blue-600"></option>
+          <option value={'#c084fc'} className="w-2 h-2 bg-purple-400"></option>
+          <option value={'#e879f9'} className="w-2 h-2 bg-fuchsia-400"></option>
+          <option value={'#f472b6'} className="w-2 h-2 bg-pink-400"></option>
+          <option value={'#fb7185'} className="w-2 h-2 bg-rose-400"></option>
+          <option value={'#000000'} className="w-2 h-2 bg-black"></option>
+          <option value={'#FFFFFF'} className="w-2 h-2 bg-white"></option>
+        </select>
+        <div className="ml-2">Highlight:</div>
+        <select
+          className="border "
+          onChange={(ev) => {
+            const value = ev.currentTarget.value;
+
+            editor.chain().focus().toggleHighlight({ color: value }).run();
+          }}
+        >
+          <option value={'#94a3b8'} className="w-2 h-2 bg-slate-400"></option>
+          <option value={'#6b7280'} className="w-2 h-2 bg-gray-500"></option>
+          <option value={'#ef4444'} className="w-2 h-2 bg-red-500"></option>
+          <option value={'#fb923c'} className="w-2 h-2 bg-orange-400"></option>
+          <option value={'#4ade80'} className="w-2 h-2 bg-green-400"></option>
+          <option value={'#2dd4bf'} className="w-2 h-2 bg-teal-400"></option>
+          <option value={'#22d3ee'} className="w-2 h-2 bg-cyan-400"></option>
+          <option value={'#2563eb'} className="w-2 h-2 bg-blue-600"></option>
+          <option value={'#c084fc'} className="w-2 h-2 bg-purple-400"></option>
+          <option value={'#e879f9'} className="w-2 h-2 bg-fuchsia-400"></option>
+          <option value={'#f472b6'} className="w-2 h-2 bg-pink-400"></option>
+          <option value={'#fb7185'} className="w-2 h-2 bg-rose-400"></option>
+          <option value={'#000000'} className="w-2 h-2 bg-black"></option>
+          <option value={'#FFFFFF'} className="w-2 h-2 bg-white"></option>
         </select>
       </div>
       <EditorContent className="prose top-0 relative" editor={editor} />
