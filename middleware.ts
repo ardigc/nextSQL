937589e94ac7dc4ca73979556f9cf3f25f8ad3d6
ardigc <130089987+ardigc@users.noreply.github.com>
2023-06-14 04:03:57 +0000

    preubas con middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,19 @@
+import { verify } from 'jsonwebtoken';
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
+  const cookiesValue = req.cookies;
+  console.log('hola');
+  let user = null;
+  try {
+    user = verify(
+      cookiesValue.get('token')?.value || '',
+      process.env.JWT_SECRET || ''
+    );
+  } catch (error: any) {
+    console.error('mensaje del middleware:', error.message);
+  }
+  console.log(user);
   return NextResponse.redirect(new URL('/products', req.url));
 }
 export const config = {
