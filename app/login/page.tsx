4e6658ce6dfebe183fbabd12859792dbf65008ce
ardigc <agordo.95@gmail.com>
2023-09-12 00:05:01 +0200

    testing responsive desing

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { FormEventHandler, useState } from 'react';
 import Link from 'next/link';
-import { SnackBar, Alert, Paper, Button } from 'gordo-ui';
+import { SnackBar, Alert, Paper, Button, TextField } from 'gordo-ui';
 export default function SignIn() {
   const [openAlert, setOpenAlert] = useState(false);
   const submitHandler: FormEventHandler<HTMLFormElement> = async (e) => {
@@ -28,34 +28,47 @@ export default function SignIn() {
   };
   return (
     <div className="relative w-full flex">
-      <Paper className=" mx-auto mt-7 border rounded-lg min-w-fit flex justify-center bg-white max-w-5xl ">
-        <div className=" m-3  grid grid-cols-2 w-full gap-3">
-          <div className="text-3xl border-b pb-2 ">Iniciar sesión</div>
-          <div className=" text-3xl border-b  pb-2">
-            <p>¿No tienes cuenta?</p>
-          </div>
-          <form onSubmit={submitHandler} className="gap-3 grid grid-cols-1 ">
-            <label>Correo electronico</label>
-            <input name="email" type="email"></input>
-
-            <label>Contraseña</label>
-            <input name="pass" type="password"></input>
-
-            <div className="flex justify-end">
-              <Button
-                disableRipple
-                className="w-full my-2"
-                color="success"
-                variant="contained"
-                size="small"
-              >
-                Iniciar Sesión
-              </Button>
-            </div>
-          </form>
+      <Paper className=" mx-auto mt-7 border rounded-lg min-w-fit flex  justify-center bg-white max-w-5xl ">
+        <div className=" m-3  grid grid-cols-1 md:grid-cols-2  w-full gap-3">
           <div>
-            Crea tu cuenta ahora y disfruta de todos los productos disponibles
-            <Link href="/register">¿No tienes cuenta?</Link>
+            <div className="text-3xl border-b pb-2 mb-2">Iniciar sesión</div>
+            <form onSubmit={submitHandler} className="gap-3 grid grid-cols-1 ">
+              {/* <label>Correo electronico</label>
+            <input name="email" type="email"></input> */}
+              <TextField
+                label="Correo Electronico"
+                name="email"
+                type="email"
+              ></TextField>
+
+              {/* <label>Contraseña</label>
+            <input name="pass" type="password"></input> */}
+              <TextField
+                name="pass"
+                type="password"
+                label="Contraseña"
+              ></TextField>
+              <div className="flex justify-end">
+                <Button
+                  disableRipple
+                  className="w-full my-2"
+                  color="success"
+                  variant="contained"
+                  size="small"
+                >
+                  Iniciar Sesión
+                </Button>
+              </div>
+            </form>
+          </div>
+          <div>
+            <div className=" text-3xl border-b  pb-2">
+              <p>¿No tienes cuenta?</p>
+            </div>
+            <div>
+              Crea tu cuenta ahora y disfruta de todos los productos disponibles
+              <Link href="/register">¿No tienes cuenta?</Link>
+            </div>
           </div>
         </div>
         <SnackBar
