e4610d0304b6e62f4cca159da5bac96264791a6c
ardigc <agordo.95@gmail.com>
2023-09-12 00:44:58 +0200

    linkedin

--- components/footer/FooterComponent.tsx
+++ components/footer/FooterComponent.tsx
@@ -1,15 +1,20 @@
 import { IconButton } from 'gordo-ui';
-import { GithubIcon } from '../Icons/Icons';
+import { GithubIcon, LinkedInIcon } from '../Icons/Icons';
 
 export default function FooterComponent() {
   return (
     <footer className="w-full mt-10">
       <div className="flex mx-auto items-center flex-col">
         <p className="text-xl">Creado por Adrián Gordo</p>
-        <div className="flex">Echa un vistazo a mis redes</div>
-        <IconButton href="https://github.com/ardigc" disableRipple>
-          <GithubIcon />
-        </IconButton>
+        <div>Echa un vistazo a mis redes</div>
+        <div className="flex">
+          <IconButton href="https://github.com/ardigc" disableRipple>
+            <GithubIcon />
+          </IconButton>
+          <IconButton href="https://www.linkedin.com/in/adrian-gordo-cortes-1375ba282/">
+            <LinkedInIcon />
+          </IconButton>
+        </div>
       </div>
     </footer>
   );
