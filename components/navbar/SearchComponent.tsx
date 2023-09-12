9350b872cfddeac3a46086744b457776badb510c
ardigc <agordo.95@gmail.com>
2023-09-12 13:13:00 +0200

    STUYLING SEARCH

--- components/navbar/SearchComponent.tsx
+++ components/navbar/SearchComponent.tsx
@@ -3,14 +3,16 @@ import { SearchIcon } from '../Icons/Icons';
 
 export default function SearchComponent() {
   return (
-    <div className="max-w-xl w-full">
+    <div className="max-w-xl w-full flex items-center">
       <TextField
         disableUnderline
         inputProps={{ startAdornment: <SearchIcon /> }}
         fullWidth
         className="bg-white  rounded-2xl "
         size="small"
-        classes={{ inputClassName: 'ml-4 pt-2 w-full hover:text-neutral-600' }}
+        classes={{
+          inputClassName: 'ml-4 [div>&]:py-1 w-full hover:text-neutral-600',
+        }}
         placeholder="¿Que necesitas?"
         // onChange={changeHandler}
         // onClick={clickHandler}
