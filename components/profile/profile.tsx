b7325de96f8c274bbd53dd6273aea757892d46bb
ardigc <agordo.95@gmail.com>
2023-09-15 12:32:55 +0200

    styling menu

--- components/profile/profile.tsx
+++ components/profile/profile.tsx
@@ -4,6 +4,7 @@ import Cart from '../cart/Cart';
 import LogOut from './LogOut';
 import { verify } from 'jsonwebtoken';
 import Link from 'next/link';
+import { Button, Menu, MenuList, Paper, Popover } from 'gordo-ui';
 
 export default function ProfileButton({
   user,
@@ -11,16 +12,63 @@ export default function ProfileButton({
   user: { id: number; name: string; role: string };
 }) {
   const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<Element | undefined>(undefined);
 
   return (
     <div className="relative">
-      <button
-        className="hover:ring-2 hover:ring-blue-800"
-        onClick={() => setOpen(!open)}
+      <Button
+        disableRipple
+        variant="text"
+        className="[div>&]:text-black"
+        onClick={(ev) => {
+          setAnchorEl(ev.currentTarget), setOpen(true);
+        }}
       >
         {user.name}
-      </button>
-      {open && (
+      </Button>
+      <Menu
+        onClose={() => {
+          setAnchorEl(undefined), setOpen(false);
+        }}
+        open={open}
+        anchorEl={anchorEl}
+      >
+        <MenuList className="m-2">
+          <li
+            onClick={() => {
+              setAnchorEl(undefined), setOpen(false);
+            }}
+          >
+            <Link href="/profile" onClick={() => setOpen(false)}>
+              Tu perfil
+            </Link>
+          </li>
+          <li
+            onClick={() => {
+              setAnchorEl(undefined), setOpen(false);
+            }}
+          >
+            <Link href="/profile/orders">Pedidos</Link>
+          </li>
+          {user.role === 'seller' && (
+            <li
+              onClick={() => {
+                setAnchorEl(undefined), setOpen(false);
+              }}
+            >
+              <Link href="/profile/products">Tus productos</Link>
+            </li>
+          )}
+          <li
+            onClick={() => {
+              setAnchorEl(undefined), setOpen(false);
+            }}
+          >
+            <LogOut />
+          </li>
+        </MenuList>
+      </Menu>
+      {/* {open && (
         <div className="absolute bg-blue-300 border border-blue-400 w-32 ">
           <ul>
             <li>
@@ -41,7 +89,7 @@ export default function ProfileButton({
             </li>
           </ul>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
