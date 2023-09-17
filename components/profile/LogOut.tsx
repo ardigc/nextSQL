77421d80ece73527396a43ae00a9f3c9eebf9e7f
ardigc <agordo.95@gmail.com>
2023-09-17 10:55:51 +0200

    update gordoui

--- components/profile/LogOut.tsx
+++ components/profile/LogOut.tsx
@@ -2,23 +2,16 @@
 
 import { LogOutIcon } from '@/components/Icons/Icons';
 
-// import { useRouter } from "next/router";
-
 export default function LogOut() {
   // const router = useRouter()
   const cerrarSesion = () => {
     document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-    // router.push('/login');
     window.location.reload();
   };
   return (
     <div className="flex items-center">
       <LogOutIcon />
-      <button
-        type="button"
-        onClick={cerrarSesion}
-        // className="px-1 my-2 border bg-blue-400 rounded-3xl ml-7"
-      >
+      <button type="button" onClick={cerrarSesion}>
         Cerrar sesión
       </button>
     </div>
